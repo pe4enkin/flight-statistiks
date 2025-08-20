c2dae8276893c434006282228714643373fe265c
pe4enkin <pe4enkin@yandex.ru>
2025-08-20 10:55:38 +0200

    Добавлен метод calculateMinFlightDurationByCarrier и приватный метод calculateFlightDuration в TicketService, в классе Main реализовано решение задачи по поиску минимального времени полета между заданными городами

--- src/main/java/com/github/pe4enkin/flightstatistics/Main.java
+++ src/main/java/com/github/pe4enkin/flightstatistics/Main.java
@@ -1,27 +1,35 @@
 package com.github.pe4enkin.flightstatistics;
 
 import com.github.pe4enkin.flightstatistics.model.Ticket;
+import com.github.pe4enkin.flightstatistics.service.TicketService;
 import com.github.pe4enkin.flightstatistics.util.JsonReader;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
+import java.time.Duration;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("tickets.json");
-        if (inputStream == null) {
-            throw new RuntimeException("Файл tickets.json не найден в resources");
+        List<Ticket> tickets;
+
+        try (InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("tickets.json")) {
+            if (inputStream == null) {
+                throw new RuntimeException("Файл tickets.json не найден в resources.");
         }
-        List<Ticket> tickets = new ArrayList<>();
-        try {
-            tickets= JsonReader.readTickets(inputStream);
+            tickets = JsonReader.readTickets(inputStream);
         } catch (IOException e) {
-            System.out.println("Неожиданная ошибка чтения: " + e.getMessage() + e);
+            throw new RuntimeException("Неожиданная ошибка чтения.", e);
         }
-        for (Ticket ticket : tickets) {
-            System.out.println(ticket);
-        }
+
+        Map<String, Duration> durationMap = TicketService.calculateMinFlightDurationByCarrier(tickets, "VVO", "TLV");
+        System.out.printf("%-10s %-15s %-15s %-10s\n", "Перевозчик", "Отправление", "Назначение", "Длительность");
+        durationMap.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue())
+                .forEach(entry -> {
+                    String flightTime = entry.getValue().toHours() + "ч. " + entry.getValue().toMinutesPart() + "м.";
+                    System.out.printf("%-10s %-15s %-15s %-10s\n", entry.getKey(), "Владивосток", "Тель-Авив", flightTime);
+                });
     }
 }
--- src/main/java/com/github/pe4enkin/flightstatistics/service/TicketService.java
+++ src/main/java/com/github/pe4enkin/flightstatistics/service/TicketService.java
@@ -1,4 +1,31 @@
 package com.github.pe4enkin.flightstatistics.service;
 
+import com.github.pe4enkin.flightstatistics.model.Ticket;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class TicketService {
+
+    public static Map<String, Duration> calculateMinFlightDurationByCarrier(List<Ticket> tickets,String origin, String destination) {
+        Map<String, Duration> durationMap =new HashMap<>();
+        for (Ticket ticket : tickets) {
+            if (ticket.getOrigin().equals(origin) && ticket.getDestination().equals(destination)) {
+                String carrier = ticket.getCarrier();
+                Duration flightDuration = calculateFlightDuration(ticket);
+                if (!durationMap.containsKey(carrier) || flightDuration.compareTo(durationMap.get(carrier)) < 0) {
+                    durationMap.put(carrier, flightDuration);
+                }
+            }
+        }
+        return durationMap;
+    }
+
+    private static Duration calculateFlightDuration(Ticket ticket) {
+        return Duration.between(LocalDateTime.of(ticket.getDepartureDate(), ticket.getDepartureTime()),
+                LocalDateTime.of(ticket.getArrivalDate(), ticket.getArrivalTime()));
+    }
 }
